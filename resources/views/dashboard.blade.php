c7e7ffa71b0861c56f6cd4bd69ade155103d592a
Martin Niz <martin.niz.niz@gmail.com>
2023-07-11 21:53:37 -0300

    post component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -49,37 +49,27 @@
   <section class="container mx-auto mt-10">
     <h2 class="text-4xl text-center font-black my-10">Posts</h2>
 
-    @if ($posts->count())
-    <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-      @foreach ($posts as $post)
-      <div>
-        <a href="{{ route('posts.show', ['post' => $post, 'user' => $user]) }}">
-          <img src="{{ asset('uploads') . '/' . $post->image}}" alt="post image {{$post->title}}">
+    <x-post-card :posts="$posts" > 
+      <x-slot:no-post>
+        @if (auth()->id() == $user->id)
+        <a href="{{ route('posts.create') }}">
+        @endif
+          <div class="border-solid border-2 border-gray-500 rounded-full flex flex-col items-center justify-center w-64 h-64 mx-auto">
+            <div class="flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-20 text-gray-600">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+              </svg>      
+            </div>
+            <p class="text-gray-600 uppercase text-center font-bold text-lg">No posts</p>
+          </div>      
+        @if (auth()->id() == $user->id)
         </a>
-      </div>
-      @endforeach
-    </div>
+        @endif
+      </x-slot:noPost>
+    </x-post-card>
 
-    <div class="my-10">
-      {{ $posts->links('pagination::tailwind') }}
-    </div>
 
-    @else
-    @if (auth()->id() == $user->id)
-    <a href="{{ route('posts.create') }}">
-    @endif
-      <div class="border-solid border-2 border-gray-500 rounded-full flex flex-col items-center justify-center w-64 h-64 mx-auto">
-        <div class="flex items-center justify-center">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-20 h-20 text-gray-600">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
-          </svg>      
-        </div>
-        <p class="text-gray-600 uppercase text-center font-bold text-lg">No posts</p>
-      </div>      
-    @if (auth()->id() == $user->id)
-    </a>
-    @endif
-    @endif
+
   </section>
 @endsection
